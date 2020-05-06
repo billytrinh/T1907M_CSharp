9db899965627d972d2aade6b95186962c8a05099
billytrinh <quanghoa.trinh@gmail.com>
2020-05-06 11:42:16 +0700

    lab 8

--- ConsoleApp1/Session8/Session8.cs
+++ ConsoleApp1/Session8/Session8.cs
@@ -9,16 +9,18 @@ namespace ConsoleApp1.Session8
         
         public static void Main(string[] args)
         {
-            Thread t1 = new Thread(ThreadRun);
-            t1.Start("xin chao");// chay luong moi
-            
-            Thread t2 = new Thread(new Session8().Run2);
-            t2.Start();
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine("main i = "+i);
-                Thread.Sleep(100);
-            }
+            Thread t3 = new Thread(Run3);
+            t3.Start(15);
+            // Thread t1 = new Thread(ThreadRun);
+            // t1.Start("xin chao");// chay luong moi
+            //
+            // Thread t2 = new Thread(new Session8().Run2);
+            // t2.Start();
+            // for (int i = 0; i < 100; i++)
+            // {
+            //     Console.WriteLine("main i = "+i);
+            //     Thread.Sleep(100);
+            // }
         }
 
         public static void ThreadRun(object o) // ko co tham so hoac  1 tham so object
@@ -38,6 +40,21 @@ namespace ConsoleApp1.Session8
                 Console.WriteLine(" xyz = "+(number+=2));
                 Thread.Sleep(100);
             }
+        }
+
+        public static void Run3(object o)
+        {
+            int n = (int) o;
+            for (int i = n-1; i >=0 ; i--)
+            {
+                for (int j = 59; j >= 0; j--)
+                {
+                    // keyword: number format 01 02 in C# 
+                    Console.WriteLine(i.ToString("D2")+":"+j.ToString("D2"));
+                    Thread.Sleep(10);
+                }
+            }
+            Console.WriteLine("Boom...");
         }
     }
 }
